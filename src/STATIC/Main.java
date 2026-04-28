1e933690c808270e34f7bf0b7d1d62c5fb227e19
talesxx3 <tales_silva2013@live.com>
2026-04-28 19:02:23 -0300

    feat: Elementos static

---
+++ src/STATIC/Main.java
@@ -0,0 +1,8 @@
+package STATIC;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Valor da soma: " + Utilidades.soma(2,3));
+        System.out.println("Valor: " + Utilidades.VALOR);
+    }
+}
---
+++ src/STATIC/Utilidades.java
@@ -0,0 +1,10 @@
+package STATIC;
+
+public class Utilidades {
+
+    public static final int VALOR = 10;
+
+    public static int soma (int  x, int y) {
+        return x + y;
+    }
+}
